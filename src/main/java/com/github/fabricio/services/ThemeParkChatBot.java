acc7a0c947a7f09e36b82d56a1ed84c8c5132b63
fabricio <fabricio.jacob@outlook.com>
2026-06-30 20:51:57 -0300

    ADD

--- src/main/java/com/github/fabricio/services/ThemeParkChatBot.java
+++ src/main/java/com/github/fabricio/services/ThemeParkChatBot.java
@@ -8,7 +8,7 @@ import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 import io.quarkiverse.langchain4j.ToolBox;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.context.SessionScoped;
+
 
 
 @RegisterAiService(retrievalAugmentor = RidesRetrievalArgumentor.class)
